4c611707f75391d64f536a3fbc11975e6d9944f8
Jaime <jpanaia@gmail.com>
2015-01-29 10:05:39 -0500

    students in pairs

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -4,3 +4,17 @@
 students = ['Daniele', 'Alesia','Anita', 'Ben', 'Thomas', 'Matthew', 'Mark', 'Jennifer','Cameron']
 
 students.shuffle!
+
+# set a count
+count = 0
+num_students = students.length - 1
+
+# until count is the number of students
+until count == num_students
+ 
+  # student pairs
+  puts "#{students[count]} & #{students[count+1]}"
+  
+  # increase the count
+  count += 2 
+end # end until
