71dc8dbe0a5b118d56051b5224b7930839d0062d
Jaime <jpanaia@gmail.com>
2015-01-29 10:37:36 -0500

    working for evens and odds

--- student_shuffle.rb
+++ student_shuffle.rb
@@ -1,20 +1,29 @@
 # Write a program that splits the class into teams of 2. 
 # If there's an odd number of students the last group should be a group of 3.
 
-students = ['Daniele', 'Alesia','Anita', 'Ben', 'Thomas', 'Matthew', 'Mark', 'Jennifer','Cameron']
+students = ['Daniele','Anita', 'Ben', 'Jojo', 'Thomas', 'Matthew', 'Mark', 'Jennifer','Cameron']
 
 students.shuffle!
 
 # set a count
 count = 0
-num_students = students.length - 1
+num_students = students.length
 
 # until count is the number of students
 until count == num_students
- 
-  # student pairs
-  puts "#{students[count]} & #{students[count+1]}"
-  
+  # odd # students
+  if num_students % 2 == 1
+    if count < num_students - 3
+      # output pairs of students
+      puts "#{students[count]} & #{students[count+1]}"
+    else
+      puts "#{students[count]}, #{students[count+1]} & #{students[count+2]}"
+      break
+    end
+  else # even # students
+    # output pairs of students
+    puts "#{students[count]} & #{students[count+1]}"
+  end
   # increase the count
   count += 2 
 end # end until
