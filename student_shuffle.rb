d893a833b86dc530794903e62332423b30b1abf8
Jaime <jpanaia@gmail.com>
2015-01-29 09:39:13 -0500

    first commit

---
+++ student_shuffle.rb
@@ -0,0 +1,6 @@
+# Write a program that splits the class into teams of 2. 
+# If there's an odd number of students the last group should be a group of 3.
+
+students = ['Daniele', 'Alesia','Anita', 'Ben', 'Thomas', 'Matthew', 'Mark', 'Jennifer','Cameron']
+
+puts students
